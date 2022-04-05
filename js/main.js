bb0758cb52b6a71fe4426b0590c7f28a3c572ba7
Akmaljon571 <ahmadjonovakmaljon571@yandex.ru>
2022-04-05 18:18:26 +0300

    karakara

---
+++ js/main.js
@@ -0,0 +1,45 @@
+let son = Number(prompt('2 dan 10 gacha bolgan son kiriting'));
+
+if (son){
+    if (son == 2){
+       for (let i = 2; i<= 10 ; i++){
+           console.log(son*i);
+       }
+    } else if (son == 3){
+        for (let i = 2; i<= 10 ; i++){
+            console.log(son*i);
+        }
+    } else if (son == 4){
+        for (let i = 2; i<= 10 ; i++){
+            console.log(son*i);
+        }
+    } else if (son == 5){
+        for (let i = 2; i<= 10 ; i++){
+            console.log(son*i);
+        }
+    } else if (son == 6){
+        for (let i = 2; i<= 10 ; i++){
+            console.log(son*i);
+        }
+    } else if (son == 7){
+        for (let i = 2; i<= 10 ; i++){
+            console.log(son*i);
+        }
+    } else if (son == 8){
+        for (let i = 2; i<= 10 ; i++){
+            console.log(son*i);
+        }
+    } else if (son == 9){
+        for (let i = 2; i<= 10 ; i++){
+            console.log(son*i);
+        }
+    } else if (son == 10){
+        for (let i = 2; i<= 10 ; i++){
+            console.log(son*i);
+        }
+    } else {
+        alert('2 dan 10gacha son kiriting')
+    }
+} else {
+    alert('son kiriting')
+}
